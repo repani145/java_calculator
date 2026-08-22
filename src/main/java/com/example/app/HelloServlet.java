b19b1ce3ecf1ea88765846ecd31f6fc2344ce582
reapni145 <repani145@gamail.com>
2026-08-22 13:10:48 +0530

    app updated as web app

---
+++ src/main/java/com/example/app/HelloServlet.java
@@ -0,0 +1,36 @@
+package com.example.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Simple Java Application</title>");
+        out.println("</head>");
+
+        out.println("<body>");
+        out.println("<h1>Hello from my Java Application!</h1>");
+        out.println("<p>Application deployed successfully on Tomcat.</p>");
+        out.println("</body>");
+
+        out.println("</html>");
+    }
+}
